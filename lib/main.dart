760872ac6cf7f1e031f29efcf6684b70e3d1d279
ahmedkmadani <ahmedkamal543@gmail.com>
2020-04-26 15:27:01 +0300

    add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,23 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title : Text('my first app'),
       centerTitle: true, 
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('Hello Ninja'),
+      child: Text(
+        "Hello Ninja",
+        style: TextStyle(
+          fontSize : 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600]
+        ),
+      ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Text("Click"),
+        backgroundColor: Colors.red[600],
       ),
   )
 ));
